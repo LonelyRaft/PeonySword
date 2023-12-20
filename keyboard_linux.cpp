44e60ab41997a970c249ecef59616c963e50e783
LonelyRaft <springsparrow9@163.com>
2023-12-20 23:58:26 +0800

    update keyevent

--- keyboard_linux.cpp
+++ keyboard_linux.cpp
@@ -92,25 +92,18 @@ namespace PeonySword {
                 event.type == EV_KEY) {
                 KeyEvent keyEvent;
                 keyEvent.mKeyValue = toKeyValue(event.code);
-                switch (event.value) {
-                    case 0: {
-                        keyEvent.mKeyState = KeyStateUp;
-                        break;
+                if (event.value == 0) {
+                    keyEvent.mKeyState = KeyStateUp;
+                    std::lock_guard lock(sDataLock);
+                    sData[keyEvent.mKeyValue].mKeeping = false;
+                } else {
+                    keyEvent.mKeyState = KeyStateDown;
+                    std::lock_guard lock(sDataLock);
+                    if (!sData[keyEvent.mKeyValue].mKeepEnable &&
+                        sData[keyEvent.mKeyValue].mKeeping) {
+                        continue;
                     }
-                    case 1: {
-                        keyEvent.mKeyState = KeyStateDown;
-                        break;
-                    }
-                    case 2: {
-                        keyEvent.mKeyState = KeyStateDown;
-                        {
-
-                        }
-                        break;
-                    }
-                    default: {
-                        break;
-                    }
+                    sData[keyEvent.mKeyValue].mKeeping = true;
                 }
                 if (keyEvent.mKeyState != KeyStateInvalid) {
                     sEventQueue.emplace(keyEvent);
@@ -123,7 +116,92 @@ namespace PeonySword {
     }
 
     const KeyValuePair Keyboard::KeyValueTable[] = {
-            {8, KeyValueBack},
+            {1,   KeyValueEscape},
+            {2,   KeyValue1},
+            {3,   KeyValue2},
+            {4,   KeyValue3},
+            {5,   KeyValue4},
+            {6,   KeyValue5},
+            {7,   KeyValue6},
+            {8,   KeyValue7},
+            {9,   KeyValue8},
+            {10,  KeyValue9},
+            {11,  KeyValue0},
+            {12,  KeyValueSub},
+            {13,  KeyValueEqual},
+            {14,  KeyValueBack},
+            {15,  KeyValueTab},
+            {16,  KeyValueQ},
+            {17,  KeyValueW},
+            {18,  KeyValueE},
+            {19,  KeyValueR},
+            {20,  KeyValueT},
+            {21,  KeyValueY},
+            {22,  KeyValueU},
+            {23,  KeyValueI},
+            {24,  KeyValueO},
+            {25,  KeyValueP},
+            {26,  KeyValueLeftBracket},
+            {27,  KeyValueRightBracket},
+            {28,  KeyValueEnter},
+            {29,  KeyValueLeftCtrl},
+            {30,  KeyValueA},
+            {31,  KeyValueS},
+            {32,  KeyValueD},
+            {33,  KeyValueF},
+            {34,  KeyValueG},
+            {35,  KeyValueH},
+            {36,  KeyValueJ},
+            {37,  KeyValueK},
+            {38,  KeyValueL},
+            {39,  KeyValueSemiconlon},
+            {40,  KeyValueQuote},
+            {41,  KeyValueTilde},
+            {42,  KeyValueLeftShift},
+            {43,  KeyValueBackslash},
+            {44,  KeyValueZ},
+            {45,  KeyValueX},
+            {46,  KeyValueC},
+            {47,  KeyValueV},
+            {48,  KeyValueB},
+            {49,  KeyValueN},
+            {50,  KeyValueM},
+            {51,  KeyValueComma},
+            {52,  KeyValuePeriod},
+            {53,  KeyValueSlash},
+            {54,  KeyValueRightShift},
+            {56,  KeyValueLeftAlt},
+            {57,  KeyValueSpace},
+            {58,  KeyValueCaps},
+            {59,  KeyValueF1},
+            {60,  KeyValueF2},
+            {61,  KeyValueF3},
+            {62,  KeyValueF4},
+            {63,  KeyValueF5},
+            {64,  KeyValueF6},
+            {65,  KeyValueF7},
+            {66,  KeyValueF8},
+            {67,  KeyValueF9},
+            {68,  KeyValueF10},
+            {70,  KeyValueScrollLock},
+            {87,  KeyValueF11},
+            {88,  KeyValueF12},
+            {97,  KeyValueRightCtrl},
+            {99,  KeyValuePrtScreen},
+            {100, KeyValueRightAlt},
+            {102, KeyValueHome},
+            {103, KeyValueUp},
+            {104, KeyValuePgUp},
+            {105, KeyValueLeft},
+            {106, KeyValueRight},
+            {107, KeyValueEnd},
+            {108, KeyValueDown},
+            {109, KeyValuePgDown},
+            {110, KeyValueInsert},
+            {111, KeyValueDelete},
+            {119, KeyValuePause},
+            {125, KeyValueStart},
+            {127, KeyValueMenu},
     };
     const int Keyboard::KeyValueTableSize =
             sizeof(KeyValueTable) / sizeof(KeyValuePair);
